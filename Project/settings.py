3ba957fcfd9b8a0b9bbfdc51cc114ef76a7214ee
unknown <pshkmr007@gmail.com>
2021-05-30 14:29:13 +0530

    pgadmin

--- Project/settings.py
+++ Project/settings.py
@@ -81,6 +81,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'vcodifydb',
+#         'USER': 'vcodify',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
